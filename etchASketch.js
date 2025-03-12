ded0bfe2c9325208bf893a532e4ee3b156ed956e
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-03-12 18:47:17 +0100

    Add basics files and main div

---
+++ etchASketch.js
@@ -0,0 +1,23 @@
+const mainDiv = document.createElement("div");
+mainDiv.classList.add("mainDiv");
+document.body.appendChild(mainDiv);
+
+const contentDiv = document.createElement("div");
+contentDiv.classList.add("contentDiv");
+mainDiv.appendChild(contentDiv);
+
+const containerButton = document.createElement("div");
+containerButton.classList.add("containerButton");
+contentDiv.appendChild(containerButton);
+
+const choiceButton = document.createElement("button");
+choiceButton.innerText = "Choose your grid";
+containerButton.appendChild(choiceButton);
+
+const gridDiv = document.createElement("div");
+gridDiv.classList.add("gridDiv");
+contentDiv.append(gridDiv);
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+gridDiv.appendChild(grid);
