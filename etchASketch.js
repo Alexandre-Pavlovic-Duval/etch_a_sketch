06b3203d6b75ad440b875385025a1652de6e41b6
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-03-13 14:20:11 +0100

    Project finished

--- etchASketch.js
+++ etchASketch.js
@@ -1,49 +1,56 @@
+// Creation of the main div
 const mainDiv = document.createElement("div");
 mainDiv.classList.add("mainDiv");
 document.body.appendChild(mainDiv);
 
+// Creation of the displaying div
 const contentDiv = document.createElement("div");
 contentDiv.classList.add("contentDiv");
 mainDiv.appendChild(contentDiv);
 
+// Creation of the button container
 const containerButton = document.createElement("div");
 containerButton.classList.add("containerButton");
 contentDiv.appendChild(containerButton);
 
+// Creation of the button
 const choiceButton = document.createElement("button");
 choiceButton.innerText = "Choose your grid";
 choiceButton.classList.add("choiceButton");
 containerButton.appendChild(choiceButton);
 
+// Creation of the grid
 const gridDiv = document.createElement("div");
 gridDiv.classList.add("gridDiv");
 contentDiv.append(gridDiv);
 
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    gridDiv.appendChild(grid);
-}
-
-const gridElements = document.querySelectorAll(".grid");
-
-gridElements.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.classList.add("black");
-    });
-});
-
 const buttonChoiceGrid = document.querySelector(".choiceButton");
 
 const handleSize = (sizeChoosed) => {
-    document.getElementsByClassName("grid").style.height = sizeChoosed / 100;
-    document.getElementsByClassName("grid").style.width = sizeChoosed / 100;
+
+    gridDiv.innerHTML = "";
+
+    // Square grid made
+    for (let i = 0; i < sizeChoosed * sizeChoosed; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+
+        // Definition of the grids' style
+        grid.style.height = `${100 / sizeChoosed}%`;
+        grid.style.width = `${100 / sizeChoosed}%`;
+
+        // Adding the mouseover
+        grid.addEventListener("mouseover", () => {
+            grid.classList.add("black");
+        });
+
+        gridDiv.appendChild(grid);
+    }
 };
 
+// Prompt to choose the size
 buttonChoiceGrid.addEventListener("click", () => {
     let size = prompt("Choose your grid size");
+    size = parseInt(size);
     handleSize(size);
 });
-
-// Vérifier que size ne soit pas un float ou un  texte (qu'il soit onvetible en int)
-// sinon reprompt
